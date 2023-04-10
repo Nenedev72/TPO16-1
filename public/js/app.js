53b10798b38eeca903a990393735e33efc338fb0
ejbergmann <javier.bergmann@jbergmann.com.ar>
2023-04-09 23:43:57 -0300

    Menu

---
+++ public/js/app.js
@@ -0,0 +1,6 @@
+let btnMenu = document.querySelector("#btnMenu");
+let mnuNav = document.querySelector("#mnuNav");
+
+btnMenu.addEventListener("click", (e)=>{
+    mnuNav.classList.toggle("collapse");
+})
