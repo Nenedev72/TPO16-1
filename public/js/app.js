86fe8df4b28b9390aef8fbfe8a6d5a2fadb819ee
ejbergmann <javier.bergmann@jbergmann.com.ar>
2023-04-13 22:46:45 -0300

    retoques menu

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,14 @@
 let btnMenu = document.querySelector("#btnMenu");
 let mnuNav = document.querySelector("#mnuNav");
+let btnServicios = document.querySelector("#btnServicios");
+let subMenu = document.querySelector("#subMenu");
+let iUpDown = document.querySelector("#iUpDown");
 
 btnMenu.addEventListener("click", (e)=>{
     mnuNav.classList.toggle("collapse");
+})
+
+btnServicios.addEventListener("click", ()=>{
+    subMenu.classList.toggle("collapse-submenu");
+    iUpDown.classList.toggle("fa-angles-up");
 })
